1af1770e8fdd6dba6679f7b63c85812dd0271bcc
Luna <luna.realm.io.bennett24@outlook.com>
2025-05-29 19:10:06 +0000

    add: Added build tutorial documentation on website

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,31 +10,3 @@ document.addEventListener('DOMContentLoaded', () => {
         bgVideo.disablePictureInPicture = true;
     }
 });
-
-// faq hamburger menu bullshit.
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.getElementById('main-nav');
-    function toggleMenu(open) {
-        nav.classList.toggle('open', open);
-        hamburger.classList.toggle('open', open);
-    }
-    hamburger.addEventListener('click', () => {
-        const isOpen = nav.classList.contains('open');
-        toggleMenu(!isOpen);
-    });
-    hamburger.addEventListener('keydown', (e) => {
-        if (e.key === 'Enter' || e.key === ' ') {
-            e.preventDefault();
-            const isOpen = nav.classList.contains('open');
-            toggleMenu(!isOpen);
-        }
-    });
-    function handleOutsideInteraction(e) {
-        if (nav.classList.contains('open') && !nav.contains(e.target) && !hamburger.contains(e.target)) {
-            toggleMenu(false);
-        }
-    }
-    document.addEventListener('mousedown', handleOutsideInteraction);
-    document.addEventListener('touchstart', handleOutsideInteraction);
-});
